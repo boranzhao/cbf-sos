9e890e5738b1d6c9c55323d703d347a700e6f81d
Pan Zhao <boranzhao9@gmail.com>
2022-09-22 21:35:41 -0500

    Debugged a 3-state numerical example and made it work in the absence of control limits

---
+++ cbf_sos.m
@@ -0,0 +1,148 @@
+function [h_fcn,h_fcn1,X_coef,Y_coef,X_fcn,Y_fcn] = cbf_sos(plant,cbf_config)
+n= plant.n; nu = plant.nu;
+A = plant.A; B = plant.B; M = plant.M;
+x = plant.x; Zx = plant.Zx;
+h0_x = plant.h0_x;
+Au = plant.Au; cx = plant.cx;
+
+deg_X = cbf_config.deg_X; deg_Y = cbf_config.deg_Y;
+X_states_index = cbf_config.X_state_index;
+deg_Lcbf = cbf_config.deg_Lcbf; 
+deg_Lu = cbf_config.deg_Lu;
+deg_Lx = cbf_config.deg_Lx;
+deg_L_X0 = cbf_config.deg_L_X0;
+
+% polynomials and matrix coefficients
+polyX = monolist(x(X_states_index),deg_X); % note that X can only depend on the states whose derivatives are not influenced by control inputs
+polyY = monolist(x,deg_Y);    
+d_polyX = jacobian(polyX,x(X_states_index));
+X_coef = sdpvar(n,n,length(polyX),'symmetric');
+Y_coef = sdpvar(nu,n,length(polyY));
+X = zeros(n); Y = zeros(nu,n);
+% dX_dx = zeros(n,n,length(X_states_index));
+for j=1:length(X_states_index)
+    dX_dx{j} = sdpvar(n,n);
+end
+% dX_dx = X_coef(:,:,1)*d_polyX(1,:);
+% dX_dx(dX_dx-dX_dx==0)=0;
+for i = 1:length(polyX)
+    X = X + X_coef(:,:,i)*polyX(i);
+    for j = 1:length(X_states_index)
+        dX_dx{j} =  dX_dx{j} - dX_dx{j}*(i==1) + X_coef(:,:,i)*d_polyX(i,j);
+    end    
+end
+
+for i = 1:length(polyY)
+    Y = Y + Y_coef(:,:,i)*polyY(i);
+end
+paras = [X_coef(:);Y_coef(:)];
+v = sdpvar(n,1); w = sdpvar(1);
+v1 = [v;w]; 
+constrs = [];
+
+% The following constraint is redundant after having constraint (5)
+% (0): positive definiteness of X in D
+% epsilon = 1e-5;
+% [L_X,c_L_X,v_L_X] = polynomial([x;v],2);
+% exp1 = v'*(X-epsilon*eye(n))*v - L_X*h0_x;
+% constrs = [sos(exp1); sos(L_X)];
+% paras = [paras; c_L_X];
+
+% (1): \dot h >=0 in D
+X_dot = zeros(n);
+for j=1:length(X_states_index)
+    X_dot = X_dot + dX_dx{j}*(A(X_states_index(j),:)*Zx);    
+end
+tmp = M*(A*X+B*Y); 
+[Lcbf,c_Lcbf,v_Lcbf] = polynomial([x;v],deg_Lcbf); %%%%%%%%%%%%%%%%%%%%%% this may need to be changed
+exp1 = v'*(X_dot-(tmp+tmp'))*v - Lcbf*h0_x;
+constrs = [constrs sos(Lcbf) sos(exp1)];
+paras = [paras; c_Lcbf];
+
+% (2): |Au*u_til|<=1;
+if cbf_config.include_input_limits 
+    exp2 = {}; Lu ={};
+    for j = 1:size(Au,1)    
+        Psi_j = [1  Au(j,:)*Y; (Au(j,:)*Y)' X];
+        [Lu{j},c_Lu,v_Lu] = polynomial([x;v1],deg_Lu);
+        exp2{j} = v1'*Psi_j*v1 - Lu{j}*h0_x;   
+        constrs = [constrs sos(exp2{j}) sos(Lu{j})];
+        paras = [paras; c_Lu];
+    end
+end
+
+% (3): Ch = {x|h(x)>=0} is in C:={x||cx*Zx}|<1};
+exp3 = {}; Lx ={};
+for i =1:size(cx,1)
+    Phi_i = [1 cx(i,:)*X; (cx(i,:)*X)' X];
+    [Lx{i},c_Lx,v_Lx] = polynomial([x;v1],deg_Lx);
+    exp3{i} = v1'*Phi_i*v1 - Lx{i}*h0_x;  
+    constrs = [constrs sos(exp3{i}) sos(Lx{i})];
+    paras = [paras; c_Lx];
+end
+
+% (4): X >= X0; 
+X0 = sdpvar(n);
+[L_X0,c_L_X0,~] = polynomial([x;v],deg_L_X0);
+% obj = -logdet(X0);                        % for SDPT3: no need to inlcude X0>=0 since this automatically assumed when using logdet function, https://yalmip.github.io/command/logdet/     
+obj = -geomean(X0);                         % for other solvers
+exp4 = v'*(X-X0)*v-L_X0*h0_x;
+% constrs = [constrs  sos(L_X0) X0<=1e-3*eye(n)];             % X0>=1e-6*eye(n)
+constrs = [constrs sos(exp4) sos(L_X0)];      % X0>=1e-6*eye(n)
+paras = [paras; c_L_X0];
+
+ops = sdpsettings('solver','mosek','sos.numblkdg',1e-6); %, ,'sos.numblkdg',1e-7 sometimes this will cause infinite loop
+[sol,v,Q,res] = solvesos(constrs,obj,ops,paras);
+max_residual = max(res)
+disp(sol.info);
+if sol.problem ==0 || sol.problem == 4 
+   % ------------------------- extract X_fcn  ------------------------
+    X_coef = value(X_coef) 
+    X_coef(abs(X_coef)<=1e-10) = 0;
+    
+    Y_coef = value(Y_coef);
+    X_fcn = zeros(n); Y_fcn = zeros(nu,n);
+    for i=1:length(polyX)
+        X_fcn = X_fcn+ X_coef(:,:,i)*polyX(i);
+    end
+    for i=1:length(polyY)
+        Y_fcn = Y_fcn+ Y_coef(:,:,i)*polyY(i);
+    end
+    X_fcn = clean(X_fcn, 1e-10);
+    Y_fcn = clean(Y_fcn, 1e-10);
+    XX = plant.x; % so that the result from "sdisplay" command uses "XX"
+    s = sdisplay(X_fcn);
+    s2 = sdisplay(Y_fcn);
+    syms XX [n 1]
+    syms X_fcn [n n]
+    syms Y_fcn [nu n]
+    for i=1:n
+        for j=1:n
+            X_fcn(i,j) = eval(s{i,j});
+        end
+    end
+    matlabFunction(X_fcn,'File','X_fcn1','Vars',{XX});
+    X_fcn = matlabFunction(X_fcn,'Vars',{XX});
+    
+    for i=1:nu
+        for j=1:n
+            Y_fcn(i,j) = eval(s2{i,j});
+        end
+    end
+    matlabFunction(Y_fcn,'File','Y_fcn1','Vars',{XX});
+    Y_fcn = matlabFunction(Y_fcn,'Vars',{XX});
+end
+h_fcn = @(x) 1- x'/X_fcn(x)*x;
+switch length(x)
+    case 2
+        h_fcn1 = @(x1,x2) h_fcn([x1;x2]); 
+    case 3
+        h_fcn1 = @(x1,x2,x3) h_fcn([x1;x2;x3]);
+    case 4
+        h_fcn1 = @(x1,x2,x3,x4) h_fcn([x1;x2;x3;x4]); 
+    case 5
+        h_fcn1 = @(x1,x2,x3,x4,x5) h_fcn([x1;x2;x3;x4;x5]);
+    case 6
+        h_fcn1 = @(x1,x2,x3,x4,x5,x6) h_fcn([x1;x2;x3;x4;x5;x6]); 
+end
+    
--- main_num2.m
+++ main_num2.m
@@ -1,25 +1,27 @@
 %% synthesis of CBF using SOS and linear-like form
 clear;
 yalmip('clear')
-% dynamics
-% an unstable linear system 
-% from  Clark, A., 2021, Verification and synthesis of control barrier
-% functions, CDC
-A = [1 0; -1 4]; B=[1;0];
-% % Wang, L., Han, D. and Egerstedt, M., 2018, Permissive barrier
-% % certificates for safe stabilization using sum-of-squares. ACC
+%%
+% dynamics from the following paper
+% Wang, L., Han, D. and Egerstedt, M., 2018, Permissive barrier
+% certificates for safe stabilization using sum-of-squares. ACC
+n = 3; nu = 2;
+x = sdpvar(n,1); x_store = x;            % for later use 
+A = [0 1 -x(3); -x(1) 0 1; -1 -2+x(2)^2 -1];
+B = [0 0;1 0;0 1];
 % A = [0  1;-1 0]; B = [0 1];
 % x = sdpvar(2,1);
-n = size(A,1); nu = size(B,2);
-x = sdpvar(n,1); 
-x_store = x; % for later use
+Zx = x; Zx_store = Zx;
+Zx_fcn = @(x) x;
+x1=x(1); x2=x(2); x3=x(3);
+
 
 % a standard nonlinear formulation
-fx = A*x; 
-dynamics = @(t,x,u) A*x+B*u;
+    
+f_fcn = @(x) [x(2)-x(3)^2;x(3)-x(1)^2;-x(1)-2*x(2)-x(3)+x(2)^3];
+dynamics = @(t,x,u) f_fcn(x)+[0;u(1);u(2)];
 
 include_input_limits = 1;
-
 % given a fixed point, rewrite the dynamics using the deviation states in linear-like form:
 % fixed point
 xstar = zeros(n,1);
@@ -32,23 +34,53 @@ M = eye(n);
 
 % settings
 deg_X = 0;
-deg_Y = 4;
-X_states_index = 2;
+deg_Y = 3;
+X_states_index = 1;
 
 % state constraints |cx*Zx}|<1; a set C:={x||cx*Zx}|<1};
-cx = x';
-gx = x'*x-1; % a more general form to express safety constraints with gx<=0
-
+cx = [-x1/8+4/8 -x2/8+2/8 -x3/8+4/8;
+    -x1/5-2/5 -x2/5-4/5 -x3/5-2/5;
+    -x1/27 -x2/27 -x3/27+12/27;
+    -x1/16 -x2/16 -x3/16-10/16];
+ % a more general form to express safety constraints with gx<=0
+gx = [-(x1^2-4*x1+x2^2-2*x2+x3^2-4*x3+8);
+     -(x1^2+2*x1+x2^2+4*x2+x3^2+2*x3+5);
+     -(x1^2+x2^2+x3^2-12*x3+27);
+     -(x1^2+x2^2+x3^2+10*x3+16)];
+syms x1 x2 x3
+gx_fcn = [-(x1^2-4*x1+x2^2-2*x2+x3^2-4*x3+8);
+     -(x1^2+2*x1+x2^2+4*x2+x3^2+2*x3+5);
+     -(x1^2+x2^2+x3^2-12*x3+27);
+     -(x1^2+x2^2+x3^2+10*x3+16)];
+q1_fcn = matlabFunction(gx_fcn(1),'Vars',{x1,x2,x3});
+q2_fcn = matlabFunction(gx_fcn(2),'Vars',{x1,x2,x3});
+q3_fcn = matlabFunction(gx_fcn(3),'Vars',{x1,x2,x3});
+q4_fcn = matlabFunction(gx_fcn(4),'Vars',{x1,x2,x3});
 % input constraints |Au*u|<1;
-Au = 1;
+Au = [1 0;0 1];
 
 % specif a set D={x|h0(x)>=0} that contains the original safety set
-h0_x = 1-(x'*x);
+h0_x = 1-0.1*(x'*x);
+
+
+
+%% visualization 
+figure(1);clf;
+h0_fcn = @(x1,x2,x3) 1-0.01*([x1,x2,x3]*[x1,x2,x3]');
+figure(1);clf;hold on;
+fimplicit3(q1_fcn,'m','EdgeColor','k','FaceAlpha',0.4,'MeshDensity',40)
+fimplicit3(q2_fcn,'m','EdgeColor','k','FaceAlpha',0.4,'MeshDensity',40)
+fimplicit3(q3_fcn,'r','EdgeColor','k','FaceAlpha',0.4)
+fimplicit3(q4_fcn,'r','EdgeColor','k','FaceAlpha',0.4)
+% fimplicit3(h0_fcn,'r');
+axis equal
+legend({'q_1(x)=0','q_2(x)=0','q_3(x)=0','q_4(x)=0'},'interpreter','latex');
+view(3)
+xlim([-5 5]);ylim([-5 5]);zlim([-5 5]);
+xlabel('x_1');ylabel('x_2');zlabel('x_3');
+
 
 %% formulate the constraitns
-Zx = x; Zx_store = Zx;
-Zx_fcn = @(x) x;
-
 plant.n = n; plant.nu = nu;
 plant.A = A; plant.B = B; plant.M = M;
 plant.x = x; plant.Zx = Zx;
@@ -62,9 +94,9 @@ cbf_config.deg_Lu = 2;
 cbf_config.deg_Lx = 2;
 cbf_config.deg_L_X0 = 2;
 cbf_config.include_input_limits = include_input_limits;
-[h_fcn,h_fcn2,X_coef,Y_coef] = cbf_sos(plant,cbf_config)
+[h_fcn,h_fcn2,X_coef,Y_coef,X_fcn,Y_fcn] = cbf_sos(plant,cbf_config)
 %% 
-figure(1);clf;
+figure(1);
 % step=.1;
 % x1= -2:step:2;
 % x2= -2:step/2:2;
@@ -78,11 +110,9 @@ figure(1);clf;
 %         I(i,j) = h_fcn(x0)>=0;
 %     end
 % end
-ellipse(1,1,0,0,0,'r');hold on;
-fimplicit(h_fcn2)
+fimplicit3(h_fcn2,'g')
 %plot of the points (x1,x2) that verify all inequalities
 % scatter(xx1(I),xx2(I),'.');
-xlim([-2 2]);
 axis equal
 
 return;
@@ -90,7 +120,7 @@ return;
 clear v v1 L_X Lcbf Lu Lx L_X0
 clear exp1 exp2 exp3 exp4 exp5
 x = x_store; 
-v = sdpvar(2,1);
+v = sdpvar(nu+1,1);
 
 if size(X_coef,3)> 1
     error('A constant X matrix has to be enforced so that h(x) is a polynominal function of x');    
@@ -113,11 +143,15 @@ for k = 1:max_iter
     alpha = 1;  constrs =[]; paras=[];
     dh_dx = jacobian(hx,x);
     epsilon = sdpvar;
-    [ux,c_u,v_u] = polynomial(x,4); % maximum: 4  best value: 4 (w/o input limits) and 
-
+    
+    deg_ux = 4;
+    [ux1,c_u1,v_u] = polynomial(x,deg_ux); %
+    c_u2 = sdpvar(length(c_u1),1); ux2 = c_u2'*v_u;
+    ux = [ux1;ux2];c_u=[c_u1;c_u2];
+    
     % (1) cbf condition
     [Lcbf,c_Lcbf,v_Lcbf] = polynomial(x,2);
-    exp1 = dh_dx*(fx+Bx*ux)+alpha*hx-Lcbf*hx-epsilon;
+    exp1 = dh_dx*(fx+B*ux)+alpha*hx-Lcbf*hx-epsilon;
     constrs = [constrs;sos(exp1) sos(Lcbf)];
     paras = [paras;epsilon;c_u; c_Lcbf];
 
@@ -141,9 +175,9 @@ for k = 1:max_iter
     end
    
     if sol.problem ==0 || sol.problem == 4 
-        c_u = value(c_u);
+        c_u1 = value(c_u1); c_u2 = value(c_u2);
         c_Lcbf = value(c_Lcbf);
-        ux = c_u'*v_u;
+        ux = [c_u1 c_u2]'*v_u;
         Lcbf = c_Lcbf'*v_Lcbf;
         if include_input_limits 
             for j = 1:size(Au,1)  
@@ -168,7 +202,7 @@ for k = 1:max_iter
     paras = [Qh(:);mu];
     ops = sdpsettings('solver','mosek','sos.numblkdg',1e-6,'verbose',0); %, ,'sos.numblkdg',1e-9 sometimes this will cause infinite loop
     % (1) cbf condition
-    exp1 = dh_dx*(fx+Bx*ux)+alpha*hx-Lcbf*hx;
+    exp1 = dh_dx*(fx+B*ux)+alpha*hx-Lcbf*hx;
     constrs = [Qh(1,1)>=1;Qh>=mu*eye(n_Zx);sos(exp1)];
 
     % (2) control limits: |Au*u|<=1;
@@ -203,6 +237,7 @@ for k = 1:max_iter
        
     end
     hx = Zx'*Qh0*Zx;  % Qh0 may be from last iteration when the iteration is failed
+    dh_dx = jacobian(hx,x);
     min_eig_Qhs(k) = min(eig(value(Qh)));
     fprintf(1,'Minimum eigenvalue of Qh: %5.2f\n',min_eig_Qhs(k))
     if max_residual > 1e-3 ||  sol.problem ~=0 
@@ -225,89 +260,112 @@ hx = clean(hx, 1e-10);
 ux = clean(ux, 1e-10);
 s_u = sdisplay(ux);
 s_h = sdisplay(hx);
+s_dh = sdisplay(dh_dx);
 syms XX [n 1]
 syms u_fcn [nu 1]
 syms h_fcn [1 1]
+syms dh_dx_fcn [1 n];
 for i=1:nu
     u_fcn(i,1) = eval(s_u{i});
 end
 h_fcn = eval(s_h{1});
+for i=1:n
+    dh_dx_fcn(i) = eval(s_dh{i}); 
+end
 % matlabFunction(u_fcn,'File','u_fcn1','Vars',{XX});
 u_fcn = matlabFunction(u_fcn,'Vars',{XX});
 h_fcn = matlabFunction(h_fcn,'Vars',{XX});
-h_fcn2 = @(x1,x2) h_fcn([x1;x2]);
+h_fcn2 = @(x1,x2,x3) h_fcn([x1;x2;x3]);
+dh_dx_fcn = matlabFunction(dh_dx_fcn,'Vars',{XX});
 
-% plot
-figure(1);
-% step=.1;
-% x1= -2:step:2;
-% x2= -2:step/2:2;
-% %z=-10:step:10;
-% %generate a grid with all triplets (x,y,z)
-% [xx1,xx2] = meshgrid(x1,x2);
-% %intersection of inequalities in a logical matrix
-% % I = (xx1.*xx1 + xx2.*xx2 < 1);
-% I = false(size(xx1));
-% for i = 1:size(xx1,1)
-%     for j=1:size(xx1,2)
-%         x0 = [xx1(i,j);xx2(i,j)];
-%         I(i,j) = h_fcn(x0)>=0;
-%     end
-% end
-
-% ellipse(1,1,0,0,0,'r');hold on;
-fimplicit(h_fcn2,'c')
-%plot of the points (x1,x2) that verify all inequalities
-% scatter(xx1(I),xx2(I),'.');
-xlim([-2 2]);
+%% plot
+figure(1);hold on;
+h_plot= fimplicit3(h_fcn2,'g','FaceAlpha',0.2,'LineStyle',':','EdgeColor','interp') %'EdgeColor','interp'
 axis equal
-legend({'g(x)=0','h(x)=0','h_{ref}(x)=0'},'interpreter','latex');
 
-%% simulate a trajectory
-x0 = [-0.6;-0.15];
-dt = 0.005;
-duration = 10;
-% simulate
-t_vec = 0:dt:duration;
-T_steps = length(t_vec)-1;
+%% simulate trajectories given initial points
+step=1;
+x1= -3:step:3;
+x2= -3:step:3;
+x3= -3:step:3;
+[xx1,xx2,xx3] = meshgrid(x1,x2,x3);
+%intersection of inequalities in a logical matrix
+% I = (xx1.*xx1 + xx2.*xx2 < 1);
+I = false(size(xx1));
+x0s = [];
+for i = 1:size(xx1,1)
+    for j=1:size(xx1,2)
+        for k= 1:size(xx1,3)
+            x0 = [xx1(i,j,k);xx2(i,j,k);xx3(i,j,k)];
+            if h_fcn(x0)>=0 
+                if isempty(x0s) || (~isempty(x0s) && all(sum((x0-x0s).^2)>2))
+                   I(i,j,k) = 1;
+                   x0s = [x0s x0];
+                end
+            end
+        end
+    end
+end
+% x0 = [-1;3;-1]; 
+% x0 = [2;-1;0];
+figure(1);
+scatter3(0,0,0,100,'r','filled','p','MarkerFaceColor','r');
+for i = 1:size(x0s,2)
+    x0 = x0s(:,i);
+    if h_fcn(x0)<0
+        error('Initial state is outside the safe region');
+    end
+    dt = 0.005;
+    duration = 20;
+    % simulate
+    t_vec = 0:dt:duration;
+    T_steps = length(t_vec)-1;
 
-% note that 
-% x: theta, alpha, q
-xTraj = zeros(n,T_steps+1);
-uTraj = zeros(nu,T_steps);
+    xTraj = zeros(n,T_steps+1);
+    uTraj = zeros(nu,T_steps);
 
-% initial condition
-x = x0;
-xTraj(:,1) = x;
+    % initial condition
+    x = x0;
+    xTraj(:,1) = x;
 
-for i=1:T_steps
-    t = t_vec(i);   
+    for i=1:T_steps
+        t = t_vec(i);   
+        % get the nominal state and input
+%         u = ustar + Y_fcn(x)/X_fcn(x)*Zx_fcn(x); % original control law
+%         u  = u_fcn(x);                             % refined control law
+        % min-norm control law with the searched cbf
+        dh_dx0 = dh_dx_fcn(x);
+        phi0 = dh_dx0*f_fcn(x) + alpha*h_fcn(x);
+        phi1 = dh_dx0*B;        
+        if phi0>=0
+            u = zeros(nu,1);
+        else
+            u = -phi1'*phi0/(phi1*phi1');
+        end 
+        % record
+        uTraj(:,i) = u;        
+        % propagate with zero-hold for control inputs
+        [d_t,d_state] = ode23(@(t,x) dynamics(t,x,u),[t_vec(i) t_vec(i+1)],x); %,ode_options
 
-    % get the nominal state and input
-    u = ustar + Y_fcn(x)/X_fcn(x)*Zx_fcn(x);
-    % record
-    uTraj(:,i) = u;        
-    % propagate with zero-hold for control inputs
-    [d_t,d_state] = ode23(@(t,x) dynamics(t,x,u),[t_vec(i) t_vec(i+1)],x); %,ode_options
-    
-    % update and record;
-    x = d_state(end,:)';    
-    xTraj(:,i+1) = x;    
-end  
+        % update and record;
+        x = d_state(end,:)';    
+        xTraj(:,i+1) = x;    
+    end  
 
-% plot the result
-figure(1)
-plot(xTraj(1,:),xTraj(2,:),'g');
-xlabel('x_1');
-xlabel('x_2');
-figure(2);
-subplot(2,1,1);
-plot(t_vec,xTraj(1,:),t_vec,xTraj(2,:));
-legend('x_1','x_2');
-ylabel('x')
-subplot(2,1,2);
-plot(t_vec(1:end-1),uTraj);
-ylabel('u')
+    % plot the result
+    figure(1)
+    plot3(xTraj(1,:),xTraj(2,:),xTraj(3,:),'k','Linewidth',1);
+    scatter3(xTraj(1,1),xTraj(2,1),xTraj(3,1),'r','Linewidth',1);
+    figure(2);
+    subplot(2,1,1);
+    plot(t_vec,xTraj(1,:),t_vec,xTraj(2,:),t_vec,xTraj(3,:));
+    legend('x_1','x_2','x_3');
+    ylabel('x')
+    subplot(2,1,2);
+    plot(t_vec(1:end-1),uTraj(1,:),t_vec(1:end-1),uTraj(2,:));
+    ylabel('u')
+    xlabel('time');
+end
 
 %% refine the CBF function by increasing the constant 1 to c(>=1) (not needed)
 % maximize c in h = c-Zx'*X^-1*Zx such that {x|h>=0} is in C
@@ -334,3 +392,23 @@ ylabel('u')
 %     h_fcn = @(x) c- x'/X_fcn(x)*x;
 % end
 
+
+%% for plot (backup)
+% step=.1;
+% x1= -2:step:2;
+% x2= -2:step/2:2;
+% %z=-10:step:10;
+% %generate a grid with all triplets (x,y,z)
+% [xx1,xx2] = meshgrid(x1,x2);
+% %intersection of inequalities in a logical matrix
+% % I = (xx1.*xx1 + xx2.*xx2 < 1);
+% I = false(size(xx1));
+% for i = 1:size(xx1,1)
+%     for j=1:size(xx1,2)
+%         x0 = [xx1(i,j);xx2(i,j)];
+%         I(i,j) = h_fcn(x0)>=0;
+%     end
+% end
+%plot of the points (x1,x2) that verify all inequalities
+% scatter(xx1(I),xx2(I),'.');
+% xlim([-2 2]);
